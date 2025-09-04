8c279f6876b1a5f7d1a6bb5575dbca0b92e891df
Dexyn <duev1473@gmail.com>
2025-09-03 23:50:05 -0500

    Going to bed now, very tired

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 import sys
 import argparse
+from functions.get_files_info import available_functions
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', action='store_true')
@@ -14,6 +15,16 @@ except SystemExit:
     print("Invalid flag or missing prompt\n|> uv run main.py 'Prompt here' [flags]")
     sys.exit(1)
     
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 user_prompt = args.user_prompt
 
 load_dotenv()
@@ -26,10 +37,16 @@ messages = [
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt)
 )
 
-print(response.text)
+if response.function_calls:
+    for call in response.function_calls:
+        print(f"Calling function: {call.name}({call.args})")
+else:
+    print(response.text)
 
 if args.verbose:
     print(f'User prompt: {user_prompt}')
